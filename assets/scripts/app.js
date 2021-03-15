4233322925bad611d877faa19cd3d12eac8d5063
Yathaartha <maharjanyathaartha017@gmail.com>
2021-03-15 19:35:49 +0545

    started adding switch logic

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,7 +1,46 @@
 class ToolTip {}
 
-class ProjectItem {}
+class ProjectItem {
+  constructor(id) {
+    this.id = id;
+    this.connectSwitchButton();
+    this.connectMoreInfoButton();
+  }
 
-class ProjectList {}
+  connectMoreInfoButton() {}
 
-class App {}
+  connectSwitchButton() {
+    const projectItemElement = document.getElementById(this.id);
+    const switchBtn = projectItemElement.querySelector("button:last-of-type");
+    switchBtn.addEventListener("click");
+  }
+}
+
+class ProjectList {
+  projects = [];
+
+  constructor(type) {
+    const prjItems = document.querySelectorAll(`#${type}-projects li`);
+    for (const prjItem of prjItems) {
+      this.projects.push(new ProjectItem(prjItem.id));
+    }
+    console.log(this.projects);
+  }
+
+  addProject() {}
+
+  switchProject(projectId) {
+    // const projectIndex = this.projects.findIndex((p) => p.id === projectId);
+    // this.projects.splice(projectIndex, 1);
+    this.projects = this.projects.filter((p) => p.id !== projectId);
+  }
+}
+
+class App {
+  static init() {
+    const activeProjectsList = new ProjectList("active");
+    const finishedProjectsList = new ProjectList("finished");
+  }
+}
+
+App.init();
